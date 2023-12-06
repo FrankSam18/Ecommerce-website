11c8f83c54daa79c9ee67be4aa91e51121c53cae
Frank  Obuobi <frank.obuobi@amalitech.org>
2023-12-06 16:38:02 +0000

    new update

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,30 +1,55 @@
 import React, { createContext, useState, useReducer, useEffect } from "react";
+import { MdOutlineCallToAction } from "react-icons/md";
 let storedCart = localStorage.getItem("cart");
 if (storedCart) storedCart = JSON.parse(storedCart);
 export const AppContext = createContext({ cart: storedCart || [] });
 
+const findItem = (existingItems, item) => {
+  return existingItems.find((value) => value.id == item.id);
+};
 const AppContextProvider = ({ children }) => {
   const reducer = (state, action) => {
     switch (action.type) {
       case "ADD":
-        console.log(action);
+        let payload = { ...action.payload, count: 1 };
+        console.log(payload);
+        if (state.cart.length === 0 || !findItem(state.cart, action.payload)) {
+          return {
+            cart: [...state.cart, payload],
+          };
+        }
+        return state;
 
+      case "INCRE":
         return {
-          cart:[action.payload, ...state.cart]
-             
+          ...state,
+          cart: state.cart.map((item) =>
+            item.id === action.payload
+              ? { ...item, count: item.count + 1 }
+              : item
+          ),
         };
-
+      case "DECRE":
+        return {
+          ...state,
+          cart: state.cart.map((item) =>
+            item.id === action.payload
+              ? { ...item, count: item.count - 1 }
+              : item
+          ),
+        };
       default:
         return state;
     }
   };
+
   const [appState, setAppState] = useReducer(reducer, {
     cart: storedCart || [],
   });
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(appState?.cart));
   }, [JSON.stringify(appState)]);
- 
+
   return (
     <AppContext.Provider value={{ ...appState, dispatch: setAppState }}>
       {children}
--- src/Pages/Viewcart/Cart.jsx
+++ src/Pages/Viewcart/Cart.jsx
@@ -4,52 +4,76 @@ import "./Cart.css";
 import { AppContext } from "../../AppContext";
 import { MdStoreMallDirectory } from "react-icons/md";
 import { Link } from "react-router-dom";
+function calculateTotalPrice(cartItems) {
+  var totalPrice = 0;
 
+  for (var i = 0; i < cartItems.length; i++) {
+    var item = cartItems[i];
+    totalPrice += item.price;
+  }
+
+  return totalPrice;
+}
 const Cart = () => {
   const { cart } = useContext(AppContext);
-
-  const renderCartItems = () => {
-    return cart.map((item) => (
-      <div className="cart-card" key={item.id}>
-        <img src={item.image} alt="product" />
-
-        <div className="title-box">
-          {item.title}
-          <div className="title-text">
-            <MdStoreMallDirectory />
-            Seller: James cottage
-            <br />
-            <p>2 Available</p>
-          </div>
-        </div>
-        <div className="price">GH₵{item.price}</div>
-        <div className="incresement-box">
-          <Link to="/Cart" className="">
-            <button className="remove-btn">-</button>
-          </Link>
-          <Link to="/Cart" className="">
-            <button className="num-btn">1</button>
-          </Link>
-          <Link to="/Cart" className="">
-            <button className="add-btn">+</button>
-          </Link>
-        </div>
-        <div className="select-box">
-          <Link to="/Products" className="remove-box">
-            <button className="remove-now-button">Remove</button>
-          </Link>
-          <Link to="/Payment" className="remove-box">
-            <button className="cart-button">Buy Now</button>
-          </Link>
-        </div>
-      </div>
-    ));
+  const totalPrice = calculateTotalPrice(cart);
+  console.log(cart);
+  const incre = (id) => {
+    dispatch({
+      type: "INCRE",
+      payload: id,
+    });
   };
-
+  const decre = (id) => {
+    dispatch({
+      type: "DECRE",
+      payload: id,
+    });
+  };
   return (
     <Layout pageNumber={1}>
-      <div className="cart-view-container">{renderCartItems()}</div>
-      <button className="check-price-button">GH₵</button>
+      <div className="cart-view-container">
+        {cart.map((item) => {
+          return (
+            <div className="cart-card" key={item.id}>
+              <img src={item.image} alt="product" />
+
+              <div className="title-box">
+                {item.title}
+                <div className="title-text">
+                  <h4>
+                    <MdStoreMallDirectory /> Seller: James cottage
+                  </h4>
+                  <br />
+                  <br />
+                  <p>2 Available</p>
+                </div>
+              </div>
+              <div className="price">GH₵{item.price}</div>
+              <div className="incresement-box">
+                <div className="addup-box">
+                  <button onClick={() => decre(item.id)} className="remove-btn">
+                    -
+                  </button>
+                  <button className="num-btn">{item.count}</button>
+                  <button onClick={() => incre(item.id)} className="add-btn">
+                    +
+                  </button>
+                </div>
+              </div>
+              <div className="select-box">
+                <Link to="/Products" className="remove-box">
+                  <button className="remove-now-button">Remove</button>
+                </Link>
+                <Link to="/Payment" className="remove-box">
+                  <button className="cart-button">Buy Now</button>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <button className="check-price-button">GH₵{totalPrice.toFixed(2)}</button>
       <Link to="/Checkout">
         <button className="check-btn">Checkout</button>
       </Link>
