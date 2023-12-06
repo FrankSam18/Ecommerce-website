06774af13ff69dc7f7cb684d5e4291e3d99f9453
Frank  Obuobi <frank.obuobi@amalitech.org>
2023-12-06 11:59:42 +0000

    update

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,20 +4,20 @@ import LandingPage from "../Pages/LandingPage/LandingPage";
 import { useNavigate } from "react-router-dom";
 
 import Cartproduct from "../Pages/Viewcart/Cart";
-import Checkout from "../Pages/checkout/Checkout";
+import Checkout from "../Pages/Checkout/Checkout";
 import Payment from "../Pages/Payment/Payment";
 import Delivery from "../Pages/delivery/Delivery";
-import Productlist from "../Pages/Productlist/Productlist"; 
+import Productlist from "../Pages/Productlist/Productlist";
 import Productdescription from "../Pages/ProductDescription/ProductDescription";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<Productlist />} />
         <Route path="/product/:id" element={<Productdescription />} />
-        <Route path="/cart" element={<Cartproduct/>} />
+        <Route path="/cart" element={<Cartproduct />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/delivery" element={<Delivery />} />
